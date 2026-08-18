736d6bebd429e6532ec825b50e54d8230f6d37d9
grazynha15 <graziela.o.silva6@aluno.senai.br>
2026-08-17 21:04:17 -0300

    teste1
    Refactor product input and removal logic.

--- controle-de-estoque.py
+++ controle-de-estoque.py
@@ -1,23 +1,19 @@
-produtos=[ ]
+produtos = []
 
-print(produtos)
 while True:
-    produto =input("Digite o nome do produto:")
+    produto = input("Digite o nome do produto:")
 
-    if produto =="sair":
+    if produto == "sair":
         print(produtos)
         break
     elif produto == "sair":
-        print(produtos)
-        break
+        produto_remover=input("digite o nome do produto que deseja remover:")
+        produtos.remove(produto_remover)
 
-   
     else:
         produtos.append(produto)
-     print(produtos)
+    print(produtos)
 
 
 
-
-   
 
